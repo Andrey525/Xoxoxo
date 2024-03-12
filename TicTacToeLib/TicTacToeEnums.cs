dcdc33c894ab7bdf92c6f80dbdc18e0fa93cb4ab
Andrey525 <ghost5run@yandex.ru>
2024-03-13 00:19:58 +0700

    Fix build

---
+++ TicTacToeLib/TicTacToeEnums.cs
@@ -0,0 +1,18 @@
+﻿namespace TicTacToeLib
+{
+    public enum TicTacToeValue
+    {
+        No,
+        X,
+        O
+    }
+    public enum TicTacToeState
+    {
+        WaitXMove = TicTacToeValue.X,
+        WaitOMove = TicTacToeValue.O,
+        XWin,
+        OWin,
+        Draw,
+        Invalid
+    }
+}
---
+++ TicTacToeLib/TicTacToeMemento.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToeLib
+{
+    public class TicTacToeMemento
+    {
+        public int MoveCount { get; private set; }
+        public TicTacToeState State { get; private set; }
+        public TicTacToeTable Table { get; private set; }
+
+        public TicTacToeMemento(int moveCount, TicTacToeState state, TicTacToeTable table)
+        {
+            MoveCount = moveCount;
+            State = state;
+            Table = table;
+        }
+    }
+}
